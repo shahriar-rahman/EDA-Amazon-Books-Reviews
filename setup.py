935c68b8d1f20460c24fe5de00f3c2c0280d86ab
Shahriar Rahman <shahriar.rahman10@northsouth.edu>
2023-06-20 05:01:19 +0600

    file update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A Deep Introspection on the characteristic properties of Concrete using a Civil Engineering Dataset. ',
+    description='An exploratory analysis of the Amazon Book Reviews. ',
     author='Shahriar',
     license='CC0: Public Domain',
 )
